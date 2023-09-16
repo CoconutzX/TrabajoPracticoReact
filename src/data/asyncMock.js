50ad4eb1e62d7fea8a53209991a6b41abc30d9d8
Ignacio Campanari <cococampanari@gmail.com>
2023-09-16 14:25:53 -0300

    Initial commit

--- src/data/asyncMock.js
+++ src/data/asyncMock.js
@@ -4,28 +4,28 @@ const travelPackages = [
       destination: 'Roma',
       description: 'Descubre la belleza histórica de Roma.',
       price: 1200,
-      image: 'ruta-a-la-imagen-de-Roma.jpg', // Reemplaza con la ruta de la imagen real
+      image: './images/roma.jpg',
     },
     {
       id: 2,
       destination: 'París',
       description: 'Explora la ciudad del amor y la luz.',
       price: 1500,
-      image: 'ruta-a-la-imagen-de-Paris.jpg', // Reemplaza con la ruta de la imagen real
+      image: './images/paris.jpg', 
     },
     {
       id: 3,
       destination: 'Nueva York',
       description: 'Vive la emoción de la Gran Manzana.',
       price: 1800,
-      image: 'ruta-a-la-imagen-de-Nueva-York.jpg', // Reemplaza con la ruta de la imagen real
+      image: './images/nuevayork.jpg', 
     },
     {
       id: 4,
       destination: 'Londres',
       description: 'Explora la capital del Reino Unido.',
       price: 1600,
-      image: 'ruta-a-la-imagen-de-Londres.jpg', // Reemplaza con la ruta de la imagen real
+      image: './images/londres.jpg',
     },
   ];
   
